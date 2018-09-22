4ecf2cb170ac543b61b57925375a9a728e1601a9
Leonel Duran <leoogle@live.com>
2018-09-22 17:33:36 -0300

    Initial

---
+++ assets/scripts/main.js
@@ -0,0 +1,299 @@
+let closes = document.querySelectorAll(".close");
+let content = document.querySelector(".content");
+let aindexs1 = document.querySelectorAll(".aindex1");
+let aindexs2 = document.querySelectorAll(".aindex2");
+let aindexs3 = document.querySelectorAll(".aindex3");
+let aindexs4 = document.querySelectorAll(".aindex4");
+let aindexs5 = document.querySelectorAll(".aindex5");
+let aindexs6 = document.querySelectorAll(".aindex6");
+let aindexs7 = document.querySelectorAll(".aindex7");
+let aindexs8 = document.querySelectorAll(".aindex8");
+let aindexs9 = document.querySelectorAll(".aindex9");
+let aindexs10 = document.querySelectorAll(".aindex10");
+let aindexs11 = document.querySelectorAll(".aindex11");
+let aindexs12 = document.querySelectorAll(".aindex12");
+let aindexs13 = document.querySelectorAll(".aindex13");
+let aindexs14 = document.querySelectorAll(".aindex14");
+let aindexs15 = document.querySelectorAll(".aindex15");
+let elementList = document.querySelectorAll(".fa-star");
+
+
+elementList.forEach(function(element){
+    element.addEventListener('click',function(){
+        element.classList.remove("far");
+        element.classList.add("fas");
+        
+    }) 
+    })
+
+
+closes.forEach(function(close){
+    close.addEventListener('click', function(ev){
+        ev.preventDefault();
+        content.classList.remove("fadeInRight");
+        content.classList.add("fadeOutRight");
+        setTimeout(() => {
+            location.href="/"
+        }, 700);
+    })
+})
+
+
+
+
+
+
+aindexs1.forEach(function(aindex1){
+    aindex1.addEventListener('click', function(ev){
+        ev.preventDefault();
+        content.classList.remove("fadeInDown");
+        content.classList.add("fadeOutUp");
+        setTimeout(() => {
+            location.href="/forbes/1.html"
+        }, 700);
+
+        
+        
+        
+        
+        
+    })
+})
+
+aindexs2.forEach(function(aindex2){
+    aindex2.addEventListener('click', function(ev){
+        ev.preventDefault();
+        content.classList.remove("fadeInDown");
+        content.classList.add("fadeOutUp");
+        setTimeout(() => {
+            location.href="/forbes/2.html"
+        }, 700);
+
+        
+        
+        
+        
+        
+    })
+})
+
+aindexs3.forEach(function(aindex3){
+    aindex3.addEventListener('click', function(ev){
+        ev.preventDefault();
+        content.classList.remove("fadeInDown");
+        content.classList.add("fadeOutUp");
+        setTimeout(() => {
+            location.href="/forbes/3.html"
+        }, 700);
+
+        
+        
+        
+        
+        
+    })
+})
+
+aindexs4.forEach(function(aindex4){
+    aindex4.addEventListener('click', function(ev){
+        ev.preventDefault();
+        content.classList.remove("fadeInDown");
+        content.classList.add("fadeOutUp");
+        setTimeout(() => {
+            location.href="/forbes/4.html"
+        }, 700);
+
+        
+        
+        
+        
+        
+    })
+})
+
+aindexs5.forEach(function(aindex5){
+    aindex5.addEventListener('click', function(ev){
+        ev.preventDefault();
+        content.classList.remove("fadeInDown");
+        content.classList.add("fadeOutUp");
+        setTimeout(() => {
+            location.href="/forbes/5.html"
+        }, 700);
+
+        
+        
+        
+        
+        
+    })
+})
+
+aindexs6.forEach(function(aindex6){
+    aindex6.addEventListener('click', function(ev){
+        ev.preventDefault();
+        content.classList.remove("fadeInDown");
+        content.classList.add("fadeOutUp");
+        setTimeout(() => {
+            location.href="/forbes/6.html"
+        }, 700);
+
+        
+        
+        
+        
+        
+    })
+})
+
+aindexs7.forEach(function(aindex7){
+    aindex7.addEventListener('click', function(ev){
+        ev.preventDefault();
+        content.classList.remove("fadeInDown");
+        content.classList.add("fadeOutUp");
+        setTimeout(() => {
+            location.href="/forbes/7.html"
+        }, 700);
+
+        
+        
+        
+        
+        
+    })
+})
+
+aindexs8.forEach(function(aindex8){
+    aindex8.addEventListener('click', function(ev){
+        ev.preventDefault();
+        content.classList.remove("fadeInDown");
+        content.classList.add("fadeOutUp");
+        setTimeout(() => {
+            location.href="/forbes/8.html"
+        }, 700);
+
+        
+        
+        
+        
+        
+    })
+})
+
+aindexs9.forEach(function(aindex9){
+    aindex9.addEventListener('click', function(ev){
+        ev.preventDefault();
+        content.classList.remove("fadeInDown");
+        content.classList.add("fadeOutUp");
+        setTimeout(() => {
+            location.href="/forbes/9.html"
+        }, 700);
+
+        
+        
+        
+        
+        
+    })
+})
+
+aindexs10.forEach(function(aindex10){
+    aindex10.addEventListener('click', function(ev){
+        ev.preventDefault();
+        content.classList.remove("fadeInDown");
+        content.classList.add("fadeOutUp");
+        setTimeout(() => {
+            location.href="/forbes/10.html"
+        }, 700);
+
+        
+        
+        
+        
+        
+    })
+})
+
+aindexs11.forEach(function(aindex11){
+    aindex11.addEventListener('click', function(ev){
+        ev.preventDefault();
+        content.classList.remove("fadeInDown");
+        content.classList.add("fadeOutUp");
+        setTimeout(() => {
+            location.href="/forbes/11.html"
+        }, 700);
+
+        
+        
+        
+        
+        
+    })
+})
+
+aindexs12.forEach(function(aindex12){
+    aindex12.addEventListener('click', function(ev){
+        ev.preventDefault();
+        content.classList.remove("fadeInDown");
+        content.classList.add("fadeOutUp");
+        setTimeout(() => {
+            location.href="/forbes/12.html"
+        }, 700);
+
+        
+        
+        
+        
+        
+    })
+})
+
+aindexs13.forEach(function(aindex13){
+    aindex13.addEventListener('click', function(ev){
+        ev.preventDefault();
+        content.classList.remove("fadeInDown");
+        content.classList.add("fadeOutUp");
+        setTimeout(() => {
+            location.href="/forbes/13.html"
+        }, 700);
+
+        
+        
+        
+        
+        
+    })
+})
+
+aindexs14.forEach(function(aindex14){
+    aindex14.addEventListener('click', function(ev){
+        ev.preventDefault();
+        content.classList.remove("fadeInDown");
+        content.classList.add("fadeOutUp");
+        setTimeout(() => {
+            location.href="/forbes/14.html"
+        }, 700);
+
+        
+        
+        
+        
+        
+    })
+})
+
+aindexs15.forEach(function(aindex15){
+    aindex15.addEventListener('click', function(ev){
+        ev.preventDefault();
+        content.classList.remove("fadeInDown");
+        content.classList.add("fadeOutUp");
+        setTimeout(() => {
+            location.href="/forbes/15.html"
+        }, 700);
+
+        
+        
+        
+        
+        
+    })
+})
